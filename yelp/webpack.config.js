b2ff0f91dd57bdbc95fd3058e575b60492e5c777
Alejandro Cortes <Alejandro Cortes>
2016-08-21 09:12:31 -0400

    added module export to web pack

--- yelp/webpack.config.js
+++ yelp/webpack.config.js
@@ -16,3 +16,35 @@ var congi = getConfig({
 	out: join(_dirname, 'dist'),
 	clearBeforeBuild: true
 })
+
+const root = resolve(_dirname);
+const src = join(root, 'src');
+const modules = join(root, 'node-modules');
+const dest = join(root, 'dist');
+
+var config = getConfig({
+	in: join(src, 'app.js'),
+	out: dest,
+	clearBeforeBuild: true
+})
+
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+// alternatively, we can use process.argv[1]
+// const isDev = (process.argv[1] || '')
+//                .indexOf('hjs-dev-server') !== -1;
+
+// ...
+var config = getConfig({
+	isDev: isDev,
+	in: join(src, 'app.js'),
+	out: dest,
+	clearBeforeBuild: true
+})
+
+// ...
+var config = getConfig({
+	// ...
+})
+
+module.exports = config;
